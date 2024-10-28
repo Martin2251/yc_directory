9cde2f526146438138a17a2812b4efee782ad201
Martin Lowe <martinlowe673@gmail.com>
2024-10-28 07:33:07 +0000

    next image

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
         <nav className='flex justify-between items-center'>
             <Link href="/">
-            <Image src="/"  />
+            <Image src="/logo.png"width={144} height={144} alt="logo" />
             </Link>
         </nav>
       <p>navbar</p>
