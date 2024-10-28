944f96eed8ef2a8d59c7ae92196969ebc101ce7e
Martin Lowe <martinlowe673@gmail.com>
2024-10-28 07:28:58 +0000

    home and navbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 export default function Home (){
   return (
     <>
+    <h2 className="text-2xl">Home</h2>
     </>
   )
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='px-5 py-3 bg-white shadow-sm'>
       Navbar
     </div>
   )
